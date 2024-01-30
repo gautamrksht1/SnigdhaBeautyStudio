3bc1888b6aa4bca59cd6187fa755ed18a9be6921
gautamrksht1 <31816417+gautamrksht1@users.noreply.github.com>
2024-01-30 21:38:55 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SnigdhaBeautyStudio.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
