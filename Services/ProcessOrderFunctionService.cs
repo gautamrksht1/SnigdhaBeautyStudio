b14ca970f70b7c845fd9cf4eb43487332d5fb147
gautamrksht1 <31816417+gautamrksht1@users.noreply.github.com>
2024-02-26 08:19:01 +0530

    commit: Added Azure function call via token authentication

--- Services/ProcessOrderFunctionService.cs
+++ Services/ProcessOrderFunctionService.cs
@@ -24,10 +24,19 @@ namespace SnigdhaBeautyStudio.Services
 
             var url = this._configuration["AzureProcessOrderFunctionApp:azureFunctionURL"];
             var functionKey = this._configuration["AzureProcessOrderFunctionApp:functionKey"];
-
+            var clientId = this._configuration["AzureProcessOrderFunctionApp:clientId"];
+            var tenantId = this._configuration["AzureProcessOrderFunctionApp:tenantId"];
+            var clientSecret = this._configuration["AzureProcessOrderFunctionApp:clientSecret"];
+            var scopes = new string[] { "api://fb9c0219-9a33-4479-9c90-b2a17385413c/.default" }; //this._configuration["AzureProcessOrderFunctionApp:scopes"];
+            _confidentialClientApplication = ConfidentialClientApplicationBuilder.Create(clientId)
+                .WithTenantId(tenantId)
+                .WithClientSecret(clientSecret)
+                .Build();
+            AuthenticationResult result = await _confidentialClientApplication.AcquireTokenForClient(scopes).ExecuteAsync();
             var httpClient = new HttpClient();
             var request = new HttpRequestMessage(HttpMethod.Post, url);
             request.Headers.Add("x-functions-key", functionKey);
+            request.Headers.Add("Authorization", result.AccessToken);
             HttpContent httpContent = new StringContent(JsonConvert.SerializeObject(content));
             request.Content = httpContent;
             var response = await httpClient.SendAsync(request);
